d71f218d7c69382c934e9795159dc96a25578919
Ethel <etheltibeb@gmail.com>
2025-01-31 23:39:39 +0200

    dbt my_project

---
+++ my_project/target/compiled/my_project/models/example/my_second_dbt_model.sql
@@ -0,0 +1,5 @@
+-- Use the `ref` function to select from other models
+
+select *
+from "Ethio_Medical_Database"."public"."my_first_dbt_model"
+where id = 1
---
+++ my_project/target/compiled/my_project/models/my_model.sql
@@ -0,0 +1,8 @@
+select
+  id,
+  text,
+  
+  current_date
+ as current_date
+from
+  "Ethio_Medical_Database"."public"."medical_database"
